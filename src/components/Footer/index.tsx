211660dfa693b72ba11e285ed1289ffd8981c41f
Pedro Ribeiro <pedrorocha@gmail.com>
2025-04-14 17:22:18 -0300

    terminando o footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,52 +1,34 @@
-import { Container, FooterSection, Link, Links, SectionTitle } from './styles'
+import { Container, Figura, Icones, Redes, Paragrafo } from './styles'
 
-const currentYear = new Date().getFullYear()
+import logo from '../../assets/images/logo.png'
+
+import insta from '../../assets/images/instagram.svg'
+
+import face from '../../assets/images/face.svg'
+
+import twitter from '../../assets/images/x.png'
 
 const Footer = () => (
   <Container>
-    <div className="container">
-      <FooterSection>
-        <SectionTitle>Categorias</SectionTitle>
-        <Links>
-          <li>
-            <Link>RPG</Link>
-          </li>
-          <li>
-            <Link>Ação</Link>
-          </li>
-          <li>
-            <Link>Aventura</Link>
-          </li>
-          <li>
-            <Link>Esportes</Link>
-          </li>
-          <li>
-            <Link>Simulação</Link>
-          </li>
-          <li>
-            <Link>Estratégia</Link>
-          </li>
-          <li>
-            <Link>FPS</Link>
-          </li>
-        </Links>
-      </FooterSection>
-      <FooterSection>
-        <SectionTitle>Acesso rápido</SectionTitle>
-        <Links>
-          <li>
-            <Link>Novidades</Link>
-          </li>
-          <li>
-            <Link>Promoções</Link>
-          </li>
-          <li>
-            <Link>Em breve</Link>
-          </li>
-        </Links>
-      </FooterSection>
-      <p>{currentYear} -&copy; E-PLAY Todos os direitos reservados</p>
-    </div>
+    <Figura>
+      <img src={logo} alt="EPLAY" />
+    </Figura>
+    <Redes>
+      <Icones>
+        <img src={insta} alt="instagram" />
+      </Icones>
+      <Icones>
+        <img src={face} alt="facebook" />
+      </Icones>
+      <li>
+        <img src={twitter} alt="twitter" />
+      </li>
+    </Redes>
+    <Paragrafo>
+      A efood é uma plataforma para divulgação de estabelecimentos, a
+      responsabilidade pela entrega, qualidade dos produtos é toda do
+      estabelecimento contratado.
+    </Paragrafo>
   </Container>
 )
 
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,32 +2,32 @@ import styled from 'styled-components'
 import { cores } from '../../styles'
 
 export const Container = styled.footer`
-  background-color: ${cores.cinza};
-  padding: 32px 0;
+  background-color: ${cores.pastel};
   font-size: 14px;
+  height: 298px;
 `
-
-export const SectionTitle = styled.h4`
-  color: ${cores.branca};
-  font-size: 16px;
-  font-weight: bold;
-`
-
-export const Links = styled.ul`
+export const Figura = styled.li`
   display: flex;
-  margin-top: 16px;
+  justify-content: center;
+  margin-top: 40px;
 `
 
-export const Link = styled.a`
-  color: ${cores.cinzaClaro};
-  text-decoration: none;
-  margin-right: 8px;
+export const Redes = styled.ul`
+  display: flex;
+  justify-content: center;
+  margin-top: 32.5px;
+`
+export const Icones = styled.li`
+  padding-right: 8px;
 `
 
-export const FooterSection = styled.div`
-  margin-bottom: 64px;
-`
-
-export const Copyright = styled.p`
-  font-size: 14px;
+export const Paragrafo = styled.p`
+  width: 480px;
+  color: ${cores.laranja};
+  font-size: 10px;
+  margin: 80px auto 40px auto;
+  font-weight: 400;
+  line-height: 100%;
+  text-align: center;
+  height: 24px;
 `
