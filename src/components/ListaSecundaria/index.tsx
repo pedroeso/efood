6db407d73946552581b0002bf23e85d79e435fb8
Pedro Ribeiro <pedrorocha@gmail.com>
2025-06-17 18:25:49 -0300

    indo para o proximo modulo

--- src/components/ListaSecundaria/index.tsx
+++ src/components/ListaSecundaria/index.tsx
@@ -1,7 +1,16 @@
+import { useState } from 'react'
 import Game from '../../models/Game'
 import ProdutoSecundario from '../ProdutoSecundario'
 
 import { Container, List } from './styles'
+
+import fundo from '../../assets/images/fundo.png'
+
+type ModalState = {
+  isVisible: boolean
+  type: 'image' | string
+  url: string
+}
 
 export type Props = {
   title: string
@@ -10,24 +19,33 @@ export type Props = {
   games: Game[]
 }
 
-const ListaSecundaria = ({ background, title, games }: Props) => (
-  <Container background={background}>
-    <div className="container">
-      <h2>{title}</h2>
-      <List>
-        {games.map((game) => (
-          <ProdutoSecundario
-            key={game.id}
-            category={game.category}
-            description={game.description}
-            image={game.image}
-            system={game.system}
-            title={game.title}
-          />
-        ))}
-      </List>
-    </div>
-  </Container>
-)
+const ListaSecundaria = ({ title, background, games }: Props) => {
+  const [modal, setModal] = useState<ModalState>({
+    isVisible: false,
+    type: 'image',
+    url: ''
+  })
+  return (
+    <>
+      <Container background={background}>
+        <div className="container">
+          <h2>{title}</h2>
+          <List>
+            {games.map((game) => (
+              <ProdutoSecundario
+                key={game.id}
+                category={game.category}
+                description={game.description}
+                image={game.image}
+                system={game.system}
+                title={game.title}
+              />
+            ))}
+          </List>
+        </div>
+      </Container>
+    </>
+  )
+}
 
 export default ListaSecundaria
--- src/components/ListaSecundaria/styles.ts
+++ src/components/ListaSecundaria/styles.ts
@@ -25,3 +25,59 @@ export const Title = styled.h2`
   font-size: 18px;
   font-weight: bold;
 `
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: none;
+  align-items: center;
+  justify-content: center;
+
+  &.visivel {
+    display: flex;
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.73);
+`
+
+export const ModalContent = styled.div`
+  max-width: 960px;
+  position: relative;
+  z-index: 1;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 24px;
+
+    h4 {
+      font-size: 24px;
+      font-weight: bold;
+    }
+  }
+
+  > img {
+    width: 100%;
+  }
+
+  img,
+  iframe {
+    display: block;
+    max-width: 100%;
+  }
+
+  iframe {
+    width: 100%;
+    height: 480px;
+  }
+`
